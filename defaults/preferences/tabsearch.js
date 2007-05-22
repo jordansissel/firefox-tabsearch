d05d1c5d2faa9fd620911c9dd829638120fe7a78
Jordan Sissel <jls@semicomplete.com>
2007-05-22 19:28:51 +0000

    - firefox extension for tab searching
    git-svn-id: https://semicomplete.googlecode.com/svn/tabsearch@1371 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ defaults/preferences/tabsearch.js
@@ -0,0 +1,5 @@
+pref("extensions.tabsearch.boolpref", false);
+pref("extensions.tabsearch.intpref", 0);
+pref("extensions.tabsearch.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.tabsearch@jordan.sissel.description", "chrome://tabsearch/locale/tabsearch.properties");
