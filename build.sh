eab8b497af7f5752958ac256fd6ae293f4b7407a
Jordan Sissel <jls@semicomplete.com>
2007-05-23 07:34:30 +0000

    - Fix build.sh to use jar not zip, and work in !linux. - Version 1.0 seems reasonable now. Works good :)
    git-svn-id: https://semicomplete.googlecode.com/svn/tabsearch@1374 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- build.sh
+++ build.sh
@@ -62,7 +62,7 @@ rm -rf $TMP_DIR
 
 $BEFORE_BUILD
 
-mkdir --parents --verbose $TMP_DIR/chrome
+mkdir -p $TMP_DIR/chrome
 
 # generate the JAR file, excluding CVS and temporary files
 JAR_FILE=$TMP_DIR/chrome/$APP_NAME.jar
@@ -71,7 +71,8 @@ for CHROME_SUBDIR in $CHROME_PROVIDERS; do
   find $CHROME_SUBDIR -path '*CVS*' -prune -o -type f -print | grep -v \~ >> files
 done
 
-zip -0 -r $JAR_FILE `cat files`
+#zip -0 -r $JAR_FILE `cat files`
+jar cf $JAR_FILE `cat files`
 # The following statement should be used instead if you don't wish to use the JAR file
 #cp --verbose --parents `cat files` $TMP_DIR/chrome
 
@@ -81,12 +82,12 @@ for DIR in $ROOT_DIRS; do
   mkdir $TMP_DIR/$DIR
   FILES="`find $DIR -path '*CVS*' -prune -o -type f -print | grep -v \~`"
   echo $FILES >> files
-  cp --verbose --parents $FILES $TMP_DIR
+  rsync $FILES $TMP_DIR
 done
 
 # Copy other files to the root of future XPI.
 for ROOT_FILE in $ROOT_FILES install.rdf chrome.manifest; do
-  cp --verbose $ROOT_FILE $TMP_DIR
+  cp -v $ROOT_FILE $TMP_DIR
   if [ -f $ROOT_FILE ]; then
     echo $ROOT_FILE >> files
   fi
@@ -101,8 +102,8 @@ if [ -f "chrome.manifest" ]; then
   #s/^(skin|locale)(\s+\S*\s+\S*\s+)(.*\/)$/\1\2jar:chrome\/$APP_NAME\.jar!\/\3/
   #
   # Then try this! (Same, but with characters escaped for bash :)
-  sed -i -r s/^\(content\\s+\\S*\\s+\)\(\\S*\\/\)$/\\1jar:chrome\\/$APP_NAME\\.jar!\\/\\2/ chrome.manifest
-  sed -i -r s/^\(skin\|locale\)\(\\s+\\S*\\s+\\S*\\s+\)\(.*\\/\)$/\\1\\2jar:chrome\\/$APP_NAME\\.jar!\\/\\3/ chrome.manifest
+  gsed -i -r s/^\(content\\s+\\S*\\s+\)\(\\S*\\/\)$/\\1jar:chrome\\/$APP_NAME\\.jar!\\/\\2/ chrome.manifest
+  gsed -i -r s/^\(skin\|locale\)\(\\s+\\S*\\s+\\S*\\s+\)\(.*\\/\)$/\\1\\2jar:chrome\\/$APP_NAME\\.jar!\\/\\3/ chrome.manifest
 
   # (it simply adds jar:chrome/whatever.jar!/ at appropriate positions of chrome.manifest)
 fi
--- content/overlay.js
+++ content/overlay.js
@@ -1,54 +1,61 @@
 var tabsearch = {
   onLoad: function() {
-    // initialization code
     this.initialized = true;
     this.strings = document.getElementById("tabsearch-strings");
-  },
-
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
+    this._console = Components.classes["@mozilla.org/consoleservice;1"].
+       getService(Components.interfaces.nsIConsoleService);
+    this._running = false;
+    this._matches = Array();
   },
 
   debug: function(val) {
-    document.getElementById("tabsearch-debug").setAttribute("value", val);
+    this._console.logStringMessage(val);
   },
 
   find: function(text) {
     var num = gBrowser.browsers.length;
     var tokens = text.split(/\s+/);
-    this.debug(tokens);
+    //this.debug(tokens);
     var re = new RegExp(text, "i");
+
+    this._matches = Array();
     for (var i = 0; i < num; i++) {
       var b = gBrowser.getBrowserAtIndex(i);
       var tab = gBrowser.mTabContainer.childNodes[i];
       if (this.find_in_browser_tab(i, tokens)) {
-        tab.setAttribute("hidden", "");
+        //this.debug(i + " matches " + tokens);
+        this.show_el(tab);
+        this._matches[this._matches.length] = tab;
       } else {
-        tab.setAttribute("hidden", "true");
+        this.hide_el(tab);
       }
     }
   },
 
   find_in_browser_tab: function(index, tokens) {
     var browser = gBrowser.getBrowserAtIndex(index);
-    var tab = gBrowser.mTabContainer.childNodes[i];
+    var tab = gBrowser.mTabContainer.childNodes[index];
+    this._matches;
     for (var i = 0; i < tokens.length; i++) {
       var re = new RegExp(tokens[i], "i");
-      if (!(re.test(browser.currentURI.spec) && re.test(tab.label))) {
+      var label = tab.label;
+      var url = browser.currentURI.spec;
+      var t_label = re.test(label);
+      var t_url = re.test(url);
+      this.debug("url: (" + t_url + ") " + url + " vs " + re);
+      this.debug("label: (" + t_label + ") " + label + " vs " + re);
+      if (!(t_label || t_url))
         return false;
-      }
     }
     return true;
   },
 
   cancel_search: function() {
+    this.debug("cancel");
     var num = gBrowser.browsers.length;
     for (var i = 0; i < num; i++) {
       var tab = gBrowser.mTabContainer.childNodes[i];
-      tab.setAttribute("hidden", "");
+      this.show_el(tab);
     }
 
     this.hide_toolbar();
@@ -57,24 +64,17 @@ var tabsearch = {
   onTabSearchKeyPress: function(e) {
     if (e.keyCode == KeyEvent.DOM_VK_RETURN) {
       var find_string = document.getElementById("tabsearch-find-field");
-      //alert("Enter. " + find_string);
       if (!find_string.value)
         return;
 
+
       //alert(find_string.value);
-      this.find(find_string.value);
-      return false;
+      //this.find(find_string.value);
+      gBrowser.mTabContainer.selectedItem = this._matches[0];
+      this.cancel_search();
     } else if (e.keyCode == KeyEvent.DOM_VK_ESCAPE) {
       this.cancel_search();
-      return false;
     }
-
-    //var find_string = document.getElementById("tabsearch-find-field").value;
-
-    //if (find_string)
-      //this.find(find_string);
-    //else 
-      //this.cancel_search();
   },
 
   onTabSearchInput: function(e) {
@@ -82,25 +82,46 @@ var tabsearch = {
 
     if (find_string)
       this.find(find_string);
-    else 
-      this.cancel_search();
+    else
+      this.debug("EMPTY!");
   },
 
   onTabSearchBlur: function(e) {
-    this.cancel_search();
+    if (this._running)
+      this.cancel_search(false);
   },
 
   show_toolbar: function() {
+    if (this._running)
+      return;
+
     var tabsearch_toolbar = document.getElementById("tabsearch-toolbar");
-    tabsearch_toolbar.setAttribute("hidden", "");
+    this.show_el(tabsearch_toolbar);
 
     var input = document.getElementById("tabsearch-find-field");
+    input.value = "";
     input.inputField.focus();
+    input.focus();
+    this._running = true;
   },
 
   hide_toolbar: function() {
+    var input = document.getElementById("tabsearch-find-field");
+    this._running = false;
+    input.inputField.blur();
+    input.blur();
+    //this.debug("Hiding...");
+
     var tabsearch_toolbar = document.getElementById("tabsearch-toolbar");
-    tabsearch_toolbar.setAttribute("hidden", "true");
+    this.hide_el(tabsearch_toolbar);
+  },
+
+  show_el: function(el) {
+    el.setAttribute("hidden", "");
+  },
+
+  hide_el: function(el) {
+    el.setAttribute("hidden", "true");
   },
 
 };
