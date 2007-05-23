d962a3162d3f51367ec830107b313c505a8f26d8
Jordan Sissel <jls@semicomplete.com>
2007-05-23 00:15:35 +0000

    Searching is mostly working. Yay XUL.
    git-svn-id: https://semicomplete.googlecode.com/svn/tabsearch@1372 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- content/overlay.js
+++ content/overlay.js
@@ -4,6 +4,7 @@ var tabsearch = {
     this.initialized = true;
     this.strings = document.getElementById("tabsearch-strings");
   },
+
   onMenuItemCommand: function(e) {
     var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
@@ -11,5 +12,103 @@ var tabsearch = {
                                 this.strings.getString("helloMessage"));
   },
 
+  debug: function(val) {
+    document.getElementById("tabsearch-debug").setAttribute("value", val);
+  },
+
+  find: function(text) {
+    var num = gBrowser.browsers.length;
+    var tokens = text.split(/\s+/);
+    this.debug(tokens);
+    var re = new RegExp(text, "i");
+    for (var i = 0; i < num; i++) {
+      var b = gBrowser.getBrowserAtIndex(i);
+      var tab = gBrowser.mTabContainer.childNodes[i];
+      if (this.find_in_browser_tab(i, tokens)) {
+        tab.setAttribute("hidden", "");
+      } else {
+        tab.setAttribute("hidden", "true");
+      }
+    }
+  },
+
+  find_in_browser_tab: function(index, tokens) {
+    var browser = gBrowser.getBrowserAtIndex(index);
+    var tab = gBrowser.mTabContainer.childNodes[i];
+    for (var i = 0; i < tokens.length; i++) {
+      var re = new RegExp(tokens[i], "i");
+      if (!(re.test(browser.currentURI.spec) && re.test(tab.label))) {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  cancel_search: function() {
+    var num = gBrowser.browsers.length;
+    for (var i = 0; i < num; i++) {
+      var tab = gBrowser.mTabContainer.childNodes[i];
+      tab.setAttribute("hidden", "");
+    }
+
+    this.hide_toolbar();
+  },
+
+  onTabSearchKeyPress: function(e) {
+    if (e.keyCode == KeyEvent.DOM_VK_RETURN) {
+      var find_string = document.getElementById("tabsearch-find-field");
+      //alert("Enter. " + find_string);
+      if (!find_string.value)
+        return;
+
+      //alert(find_string.value);
+      this.find(find_string.value);
+      return false;
+    } else if (e.keyCode == KeyEvent.DOM_VK_ESCAPE) {
+      this.cancel_search();
+      return false;
+    }
+
+    //var find_string = document.getElementById("tabsearch-find-field").value;
+
+    //if (find_string)
+      //this.find(find_string);
+    //else 
+      //this.cancel_search();
+  },
+
+  onTabSearchInput: function(e) {
+    var find_string = document.getElementById("tabsearch-find-field").value;
+
+    if (find_string)
+      this.find(find_string);
+    else 
+      this.cancel_search();
+  },
+
+  onTabSearchBlur: function(e) {
+    this.cancel_search();
+  },
+
+  show_toolbar: function() {
+    var t = document.getElementById("tabsearch-toolbar");
+    tabsearch_toolbar.setAttribute("hidden", "");
+
+    var input = document.getElementById("tabsearch-find-field");
+    input.focus();
+  }
+
+  hide_toolbar: function() {
+    var tabsearch_toolbar = document.getElementById("tabsearch-toolbar");
+    tabsearch_toolbar.setAttribute("hidden", "true");
+  }
+
 };
+
 window.addEventListener("load", function(e) { tabsearch.onLoad(e); }, false);
+
+        //var an = document.getAnonymousNodes(tab);
+        //for (var j = 0; j < an.length; j++) {
+          //an[j].style.setProperty("background-color", "red", "");
+        //}
+        //tab.style.setProperty("background-color", "red", "");
